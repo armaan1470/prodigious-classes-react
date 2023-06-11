d02100250d0bd32cbcc634d7810da14749dec3d1
razibravo76 <razibravo76@gmail.com>
2023-06-11 20:57:58 +0530

    about ib banner animation added

--- src/pages/AboutIB/AboutIB.jsx
+++ src/pages/AboutIB/AboutIB.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Footer from "../../components/Footer/Footer";
 import "./Aboutib.css";
 import logo from "../../assets/images/aboutib_logo.png";
+import ilust1 from "../../assets/images/aboutin_illust1.png"
 
 const AboutIB = () => {
+
+    const bannerText = useRef(null);
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            bannerText.current.classList.add('slide_up');
+        }, 700);
+    }, []);
+
   return (
     <>
       <div className="aboutib">
-        <div className="aboutIB_banner_text">
+        <div className="aboutIB_banner">
           <img src={logo} alt="logo" />
-          <h1>About the IB</h1>
+          <div class="aboutIB_banner_text" ref={bannerText}>About the IB</div>
         </div>
-        <div className="aboutIB_text">
-          <div>
-            <h1>
-              A variety of <br/><span> Educational Programs </span> <br />
-              Offered By IB
-            </h1>
+        <div className="aboutIB_content">
+          <div className="aboutIB_text">
+            <div>
+              <h1>
+                A variety of <br />
+                <span> Educational Programs </span> <br />
+                Offered by IB
+              </h1>
+            </div>
+            <div>
+              <p>
+                The International Baccalaureate is a global organization that
+                oversees a variety of educational programs for pupils aged three
+                to nineteen. Its programs are taught by carefully trained
+                teachers in accredited schools all around the world, and it
+                prides itself on giving pupils high quality education.
+                <br /> <br />
+                The IB programs encourage variety, international mindedness,
+                curiosity, and a healthy desire for learning and excellence, as
+                well as critical thinking and problem-solving skills.
+              </p>
+            </div>
           </div>
-          <div>
-            <p>
-              Private group tution at your door step. <br /> We provide a
-              flexible learning solution that <br /> makes learning easy and
-              convinient
-            </p>
+          <div className="aboutIB_ilust1">
+            <img src={ilust1} alt="anosadfa" width="400px"/>
           </div>
         </div>
       </div>
