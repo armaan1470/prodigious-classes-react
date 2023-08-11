f21afbe81f52f27be19e37a654382965fba91774
razibravo76 <razibravo76@gmail.com>
2023-08-11 19:12:15 +0530

    changes in igcse page

--- src/pages/IntroIG/IntroIG.jsx
+++ src/pages/IntroIG/IntroIG.jsx
@@ -22,15 +22,15 @@ const IntroIG = () => {
         </div>
       </div>
       <div className="intro_igsce">
-        <div className="igsce_ilust_Container">
-            <img src={ilust1} alt="ilustration" />
+        <div className="igsce_ilust_container">
+          <img src={ilust1} alt="ilustration" />
         </div>
         <div className="igsce_content">
-          <div>
+          <div className="content_header">
             <p>IGCSE TUTION</p>
             <h1>Introduction to IGCSE</h1>
           </div>
-          <div>
+          <div className="content-detail">
             <p>
               The International General Certificate of Secondary Education
               (IGCSE) is an internationally recognized curriculum and
@@ -55,6 +55,37 @@ const IntroIG = () => {
               education or seek employment opportunities abroad.
             </p>
           </div>
+        </div>
+      </div>
+      <div className="intro_igsce">
+        <div className="igsce_content">
+          <div className="content-detail">
+            <p>
+              pecialization. Upon completing their IGCSEs, students can progress
+              to the next level of education, such as the International
+              Baccalaureate (IB) Diploma Programme, A-Levels, or other
+              pre-university courses. These qualifications can open doors to
+              universities and colleges around the world.
+              <br />
+              <br />
+              The IGCSE curriculum also equips students with transferable skills
+              that are highly valued in the job market. These skills include
+              critical thinking, problem-solving, effective communication, and
+              independent research abilities. Graduates with IGCSE
+              qualifications may find employment opportunities in various fields
+              such as business, finance, engineering, healthcare, arts, and
+              more.
+              <br/><br/>
+              Ultimately, the IGCSE curriculum provides students with a
+              well-rounded education, preparing them for further studies and
+              future career prospects. Its international recognition and
+              emphasis on developing key skills make it a popular choice for
+              students seeking a globally recognized qualification.
+            </p>
+          </div>
+        </div>
+        <div className="igsce_ilust_container">
+          <img src={ilust2} alt="ilustration" />
         </div>
       </div>
     </div>
